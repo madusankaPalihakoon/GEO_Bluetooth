5d3ae16650cd594a8fa75a8498edce5a28e5dc81
Madusanka Palihakoon <pmsmpalihakoon@gmail.com>
2025-05-11 16:11:41 +0530

    bluetooth connection

--- app/src/main/java/com/example/geo/MainActivity.kt
+++ app/src/main/java/com/example/geo/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.geo
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -72,7 +77,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
     private fun btScan() {
-        Toast.makeText(this, "Bluetooth Connected Successfully", Toast.LENGTH_LONG).show()
+        //Toast.makeText(this, "Bluetooth Connected Successfully", Toast.LENGTH_LONG).show()
+        val inflater = layoutInflater
+        val dialogView:View = inflater.inflate(R.layout.scan_bt,null)
+        builder.setCancelable(false)
+        builder.seView(dialogView)
+        val btlist = dialogView.findViewById<ListView>(R.id.bt_list)
+        val dialog = builder.create()
+        val paireDevices:Set<BluetoothDevice> = bluetoothAdapter?.bondedDevices as Set<BluetoothDevice>
+        val ADAhere:SimpleAdapter
+        var data:MutableList<Map<String?,Any?>?>? = null
+        data = ArrayList()
+        if(paireDevices.isNotEmpty()){
+            val datanum1: MutableMap<String?,Any?> = HashMap()
+            datanum1["A"] = ""
+            datanum1["B"] = ""
+            data.add(datanum1)
+            for (device in paireDevices){
+                val datanum:MutableMap<String?,Any?> = HashMap()
+                datanum["A"] = device.name
+                datanum["B"] = device.address
+                data.add(datanum)
+            }
+            val fromWare = arrayOf("A")
+            val viewswhere  = intArrayOf(R.id.item_name)
+            ADAhere = SimpleAdapter(this@MainActivity,data,R.layout.item_list,fromWare,viewswhere)
+            btlst.adapter = ADAhere
+            ADAhere.notifyDataSetChanged()
+            btlst.onItemClickListner = AdapterView.OnItemClickListener{adapterView,view,position,l ->
+             val string = ADAhere.getItem(position) as HashMap<String,String>
+                val deviceName = string["A"]
+                binding.deviceName.text = deviceName
+                dialog.dismiss()
+            }
+        }else{
+            val value = "No Devices found"
+            Toast.makeText(this,value,Toast.LENGTH_LONG).show()
+            return
+        }
+        dialog.show()
     }
 }
